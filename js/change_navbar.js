75795fa91401e0493293672e22fc91010b6339b8
Iryna Dubyak <irynadubyak@gmail.com>
2017-11-16 17:28:26 +0200

    navbar

--- js/change_navbar.js
+++ js/change_navbar.js
@@ -1,35 +1,38 @@
-$(document).ready(function(){       
-   var scroll_start = 0;
-   var startchange = $('#start_change');
-   var offset = startchange.offset();
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $('#navbar').css('background-color', '#0f1011');
-          $('#navbar').css('opacity', '0.8');
-          $('#navbar').css('padding-top', '15px');
-          $('#navbar').css('padding-bottom', '15px');
-       } else {
-          $('#navbar').css('background-color', 'transparent');
-          $('#navbar').css('opacity', '1');
-          $('#navbar').css('padding-top', '40px');
-          $('#navbar').css('padding-bottom', '40px');
-       }
-   });
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
 
-   $('.up_button').hide();
- $(window).scroll(function() {
-        if($(this).scrollTop() > 20) {
+    if (scroll >= 100) {
+        $("#navbar").addClass("scrolling");
+    } else {
+        $("#navbar").removeClass("scrolling");
+    }
+});
+
+$(document).ready(function(){    
+  $('.up_button').hide();
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 20) {
       $('.up_button').fadeIn();
     } else {
       $('.up_button').fadeOut();
     }
-    });
-    $('.up_button').click(function() {
+  });
+
+  $('.up_button').click(function() {
     $('body, html').animate({scrollTop: 0}, 800);
   });
-    $('.down_button').click(function() {
+
+  $('.down_button').click(function() {
     $('body, html').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
-    
 });
+
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if ((x.className === "navbar_menu")||(x.className === "navbar_menu scrolling")) {
+        x.className += " responsive";
+    } 
+    else {
+        x.className = "navbar_menu scrolling";
+    }
+}
